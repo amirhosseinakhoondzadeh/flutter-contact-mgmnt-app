942b50c5a38bc4aa965743fac90041075c2cfad2
Amirhossein Akhoondzadeh <amirhossein.akhoondzadeh@live.com>
2020-06-23 09:48:54 +0430

    - Added assets

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String APP_NAME = "A Job Thing Test";
+
+  /// Fonts
+  static const String APP_FONT = "Montserrat";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:a_job_thing_test/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,21 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: AppConstants.APP_NAME,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        fontFamily: AppConstants.APP_FONT,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
